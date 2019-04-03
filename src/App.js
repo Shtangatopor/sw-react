0e1a2a4ae3d3a0945629bf9a0f8714a6dabdea03
otaky <shtangatopor@gmail.com>
2019-04-03 15:07:47 +0700

    add material ui  and button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Времена года в России
-          </p>
-        </header>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Времена года в России</p>
+                </header>
+
+                <div className='button'>
+                    <Button>Зима</Button>
+                    <Button>Весна</Button>
+                    <Button>Лето</Button>
+                    <Button>Осень</Button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
