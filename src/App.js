b900759681757a89583ec2c033fd7f1208d301c6
otaky <shtangatopor@gmail.com>
2019-04-03 15:11:08 +0700

    add WEATHER_DATA - collection of image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import {Button} from "@material-ui/core";
 
+const WEATHER_DATA = {
+    winter : 'https://www.varlamov.me/2016/snegopad/24.jpg',
+    spring : 'https://2ch.hk//b/src/188214838/15450275487203.jpg',
+    summer : 'https://varlamov.me/2018/murmansk_ploh/00s.jpg',
+    autumn : 'https://varlamov.me/2017/piter_dirt/12.jpg',
+};
+
 class App extends Component {
 
     render() {
@@ -19,7 +26,11 @@ class App extends Component {
                     <Button>Лето</Button>
                     <Button>Осень</Button>
                 </div>
+                <div>
+                    <h3>image</h3>
+                </div>
             </div>
+
         );
     }
 }
