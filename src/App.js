e525c98547417d55eb22c9a523ed550dcf2432f2
otaky <shtangatopor@gmail.com>
2019-04-05 11:33:13 +0700

    replace logic element

--- src/App.js
+++ src/App.js
@@ -1,62 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import {Button} from "@material-ui/core";
-import ImageLoader from 'react-load-image';
-
-const WEATHER_DATA = {
-    winter : 'https://www.varlamov.me/2016/snegopad/24.jpg',
-    spring : 'https://2ch.hk//b/src/188214838/15450275487203.jpg',
-    summer : 'https://varlamov.me/2018/murmansk_ploh/00s.jpg',
-    autumn : 'https://varlamov.me/2017/piter_dirt/12.jpg',
-};
-
-function Preloader(props) {
-    return <p >loading...</p>;
-}
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 
-class App extends Component {
-    state = {
-        season: 'winter',
-    };
-
-    handleChange = (e) => {
-        this.setState({
-            season: e.currentTarget.value,
-        });
-    };
-
-    render() {
-        const {season} = this.state;
-
-        return (
-            <div className="App">
-
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Времена года в России</p>
-                </header>
-
-                <div className='button'>
-                    <Button value={'winter'} onClick={this.handleChange}>Зима</Button>
-                    <Button value={'spring'} onClick={this.handleChange}>Весна</Button>
-                    <Button value={'summer'} onClick={this.handleChange}>Лето</Button>
-                    <Button value={'autumn'} onClick={this.handleChange}>Осень</Button>
-                </div>
-
-                <div>
-                    <ImageLoader src={WEATHER_DATA[season]}>
-                        <img alt={WEATHER_DATA[season]}/>
-                        <div>Error!</div>
-                        <Preloader />
-                    </ImageLoader>
-                </div>
-
-            </div>
-
-        );
-    }
-}
+const App = () => (
+    <div className="App">
+        <Header/>
+        <Main/>
+    </div>
+);
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+
+const Header = () => (
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Времена года в России</p>
+    </header>
+
+);
+
+export default Header;
