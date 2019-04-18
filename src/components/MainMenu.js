9ce9368d88d5cf3a707ca5a37b739919e3dccfa6
otaky <shtangatopor@gmail.com>
2019-04-18 14:01:17 +0700

    fix drawer with hook

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -3,21 +3,17 @@ import {AppBar, Drawer, IconButton, Toolbar, Typography} from "@material-ui/core
 import MenuIcon from '@material-ui/icons/Menu';
 import {Link} from "react-router-dom";
 
-
-function MainMenu(){
+const MainMenu = () => {
     const [open, toggleDrawer] = useState(false);
         return(
             <div>
                 <AppBar position='static'>
                     <Toolbar>
-                        <IconButton
-                            onClick={() => {toggleDrawer(true)}}
-                        ><MenuIcon/></IconButton>
+                        <IconButton onClick={() => {toggleDrawer(true)}}><MenuIcon/></IconButton>
                         <Drawer anchor='top' open={open} onClose={() => {toggleDrawer(false)}}>
                             <div>
                                 <Link to={'/expected'}><h5>expected</h5></Link>
                                 <Link to={'/reality'}><h5>reality</h5></Link>
-                                <h5>2 item</h5>
                             </div>
                         </Drawer>
                         <Typography variant='h5' color='inherit'>App bar</Typography>
