fc1ea9a3d6d8837af010bbd7a739e98859e3f9a4
otaky <shtangatopor@gmail.com>
2019-04-19 11:42:10 +0700

    submenu as link

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,8 +2,7 @@ import React, {useState} from "react";
 import {AppBar, Drawer, IconButton, ListItem, Toolbar, Typography} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import {Link} from "react-router-dom";
-import List from "@material-ui/core/List";
-import ListItemIcon from '@material-ui/core/ListItemIcon';
+import {List, ListItemText } from "@material-ui/core/List";
 
 const MainMenu = () => {
     const [open, toggleDrawer] = useState(false);
@@ -14,22 +13,9 @@ const MainMenu = () => {
                 <Toolbar>
                     <IconButton onClick={() => {toggleDrawer(true)}}><MenuIcon/></IconButton>
                     <Drawer anchor='left' open={open} onClose={close}>
-                        <List>
-                            <a href='/'>
-                                <ListItem button onClick={close}>Home</ListItem>
-                            </a>
-                            <ListItem button >
-                                <ListItemIcon>
-                                    <Link to={'/expected'} onClick={close}>Expected</Link>
-                                </ListItemIcon>
-                            </ListItem>
-
-                            <ListItem button  onClick={close}>
-                                <ListItemIcon>
-                                    <Link to={'/reality'} >Reality</Link>
-                                </ListItemIcon>
-                            </ListItem>
-                        </List>
+                        <Link to='/' onClick={close}>Home<ListItem button /></Link>
+                        <Link to='/expected' onClick={close}>Expected<ListItem button /></Link>
+                        <Link to='/reality' onClick={close}>Reality<ListItem button /></Link>
                     </Drawer>
                     <Typography variant='h5' color='inherit'>App bar</Typography>
                 </Toolbar>
